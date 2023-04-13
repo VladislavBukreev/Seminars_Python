2338fa01441d0efee8299f3302f851344bcaa0f2
VladislavBukreev <bukr7@mail.ru>
2023-04-13 14:38:58 +1000

    1

---
+++ Seminars_1/1.py
@@ -0,0 +1,12 @@
+# вводится два числа определить большее из них
+
+a = int(input('число а '))
+b = int(input('число b '))
+
+if a > b:
+    print( f"a = {a} ")
+
+elif a < b:
+    print( f"b = {b} ")
+else:
+    print("==")
